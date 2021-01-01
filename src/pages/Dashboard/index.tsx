c18f335377e7d3e300affc6d2ebeac63868d6826
Alan Christian <alanchristiancreator@hotmail.com>
2021-01-01 13:46:55 -0300

    using styled components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import { Title } from './styles';
+
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard Foda Topppp</h1>;
+  return <Title>Explore repositórios no Github</Title>;
 };
 
 export default Dashboard;
